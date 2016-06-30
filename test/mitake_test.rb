21acadfb061c3b93b530062fc35e74c0308309c1
odin <odin@shenlearn.com>
2016-06-30 18:08:28 +0800

    mitake init

---
+++ test/mitake_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class MitakeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Mitake::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
